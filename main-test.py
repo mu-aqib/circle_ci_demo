ee9f3e37e962286be5145c97ced9913acef96157
Muhammad Aqib <aqibafridi0345@gmail.com>
2022-12-02 09:13:14 +0500

    Add another addition for testing result in Circle CI

--- main-test.py
+++ main-test.py
@@ -1,8 +1,11 @@
 from main import Add
 
+
 def TestAdd():
-    assert Add(1,2) == 3
+    assert Add(1, 2) == 3
+    assert Add(5, 5) == 111
     print("Add function works properly!")
+
 
 if __name__ == '__main__':
     TestAdd()
